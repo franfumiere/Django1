3a87953fa15ab5168f4db4e4fd9442f65db39109
franfumiere <112140321+franfumiere@users.noreply.github.com>
2022-08-25 22:38:20 -0300

    Add files via upload

---
+++ Django_1/Proyecto1/Proyecto1/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+#def saludar (request): 
+    #return HttpResponse ("Proceda")
+
+#def faqs (request):
+    #return HttpResponse("<h1> Frequence Answers and Questions </h1>")
